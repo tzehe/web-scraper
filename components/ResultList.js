76a3b9e327dc1418bd21db5e872c07259a108cba
tzehe <thomas.zehe@scout24.com>
2018-11-11 02:54:02 +0100

    add: responsive ResultList

---
+++ components/ResultList.js
@@ -0,0 +1,49 @@
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 320,
+  },
+});
+
+const ResultList = ({ results, classes }) => {
+  const { data } = results;
+  console.log(data);
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Word</TableCell>
+            <TableCell numeric>Frequency</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(row => {
+            return (
+              <TableRow key={row.word}>
+                <TableCell component="th" scope="row">
+                  {row.word}
+                </TableCell>
+                <TableCell numeric>{row.frequency}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+};
+export default withStyles(styles)(ResultList);
